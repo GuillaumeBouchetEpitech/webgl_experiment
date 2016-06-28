79aafb004d5f46f502ddf54bb7fc81195773b93b
GuillaumeBouchetEpitech <guillaume.bouchet@epitech.eu>
2016-06-28 22:15:44 +0100

    clean-up

--- app/generation/chunkGenerator.js
+++ app/generation/chunkGenerator.js
@@ -142,116 +142,116 @@ define(
 
 	chunkRenderer.prototype.update = function(camera_pos, priority_cb) {
 
-		//TODO : handle the processing chunk
+		// are we already processing a chunk?
 
 		if (this.is_processing_chunk)
-			return this._partially_generate(this.processing_pos);
+			return this._partially_generate(this.processing_pos); // yes...
 
+		// no -> determine the next chunk to process
+
+		// is there something to process?
+		if (this._chunk_queue.length == 0)
+			return; // no
+
+		var pos = null;
+
+		// if just 1 chunk left to process (or no priority callback)
+		// -> just pop and process the last chunk in the queue
+		if (!priority_cb || this._chunk_queue.length == 1)
 		{
-			// nothing to process
-			if (this._chunk_queue.length == 0)
-				return;
+			pos = this._chunk_queue.pop();
 
-			var pos = null;
+			this.processing_pos = pos;
 
-			if (!priority_cb || this._chunk_queue.length == 1)
-			{
-				pos = this._chunk_queue.pop();
-
-				this.processing_pos = pos;
-
-				this.is_processing_chunk = true;
-			}
-			else
-			{
-				var best_index = 0;
-
-				//
-				///
-
-				function calc_length(in_x, in_y, in_z) {
-					return Math.sqrt(in_x*in_x + in_y*in_y + in_z*in_z);
-				}
-
-				function compute_chunk_center(pos) {
-					return [
-						pos[0] * this._chunk_size + this._chunk_size / 2,
-						pos[1] * this._chunk_size + this._chunk_size / 2,
-						pos[2] * this._chunk_size + this._chunk_size / 2
-					];
-				}
-
-				var chunks = this._chunks;
-				function is_already_processed(pos) {
-					for (var j = 0; j < chunks.length; ++j)
-						if (chunks[j].pos[0] === pos[0] &&
-							chunks[j].pos[1] === pos[1] &&
-							chunks[j].pos[2] === pos[2])
-						{
-							return true;
-						}
-					return false;
-				}
-
-				var pos = this._chunk_queue[0];
-
-				var chunk_center = [
-					pos[0] * this._chunk_size + this._chunk_size / 2,
-					pos[1] * this._chunk_size + this._chunk_size / 2,
-					pos[2] * this._chunk_size + this._chunk_size / 2
-				];
-
-				var best_dist = calc_length(camera_pos[0] - chunk_center[0],
-											camera_pos[1] - chunk_center[1],
-											camera_pos[2] - chunk_center[2]);
-
-				///
-				//
-
-				for (var i = 1; i < this._chunk_queue.length; ++i)
-				{
-					var best_pos = this._chunk_queue[best_index];
-					var try_pos = this._chunk_queue[i];
-
-					//
-					/// already processed ?
-					if (is_already_processed(try_pos))
-					{
-						this._chunk_queue.splice(i,1);
-						--i;
-						continue;
-					}
-					/// /already processed ?
-					//
-
-					if (priority_cb( try_pos, best_pos ))
-					{
-						var chunk_center = [
-							try_pos[0] * this._chunk_size + this._chunk_size / 2,
-							try_pos[1] * this._chunk_size + this._chunk_size / 2,
-							try_pos[2] * this._chunk_size + this._chunk_size / 2
-						];
-
-						var dist = calc_length( camera_pos[0] - chunk_center[0],
-												camera_pos[1] - chunk_center[1],
-												camera_pos[2] - chunk_center[2] );
-
-						if (best_dist < dist)
-							continue;
-
-						best_index = i;
-						best_dist = dist;
-					}
-				}
-
-				this.processing_pos = this._chunk_queue[best_index];
-				this._chunk_queue.splice(best_index,1);
-
-				this.is_processing_chunk = true;
-			}
+			this.is_processing_chunk = true;
 
 			return;
 		}
+
+		var best_index = 0;
+
+		//
+		///
+
+		function calc_length(in_x, in_y, in_z) {
+			return Math.sqrt(in_x*in_x + in_y*in_y + in_z*in_z);
+		}
+
+		// function compute_chunk_center(pos) {
+		// 	return [
+		// 		pos[0] * this._chunk_size + this._chunk_size / 2,
+		// 		pos[1] * this._chunk_size + this._chunk_size / 2,
+		// 		pos[2] * this._chunk_size + this._chunk_size / 2
+		// 	];
+		// }
+
+		var chunks = this._chunks;
+		function is_already_processed(pos) {
+			for (var j = 0; j < chunks.length; ++j)
+				if (chunks[j].pos[0] === pos[0] &&
+					chunks[j].pos[1] === pos[1] &&
+					chunks[j].pos[2] === pos[2])
+				{
+					return true;
+				}
+			return false;
+		}
+
+		var pos = this._chunk_queue[0];
+
+		var chunk_center = [
+			pos[0] * this._chunk_size + this._chunk_size / 2,
+			pos[1] * this._chunk_size + this._chunk_size / 2,
+			pos[2] * this._chunk_size + this._chunk_size / 2
+		];
+
+		var best_dist = calc_length(camera_pos[0] - chunk_center[0],
+									camera_pos[1] - chunk_center[1],
+									camera_pos[2] - chunk_center[2]);
+
+		///
+		//
+
+		for (var i = 1; i < this._chunk_queue.length; ++i)
+		{
+			var best_pos = this._chunk_queue[best_index];
+			var try_pos = this._chunk_queue[i];
+
+			//
+			/// already processed ?
+			if (is_already_processed(try_pos)) // <- simply look if already a "live chunk"
+			{
+				this._chunk_queue.splice(i,1);
+				--i;
+				continue;
+			}
+			/// /already processed ?
+			//
+
+			if (priority_cb( try_pos, best_pos ))
+			{
+				var chunk_center = [
+					try_pos[0] * this._chunk_size + this._chunk_size / 2,
+					try_pos[1] * this._chunk_size + this._chunk_size / 2,
+					try_pos[2] * this._chunk_size + this._chunk_size / 2
+				];
+
+				var dist = calc_length( camera_pos[0] - chunk_center[0],
+										camera_pos[1] - chunk_center[1],
+										camera_pos[2] - chunk_center[2] );
+
+				if (best_dist < dist)
+					continue;
+
+				best_index = i;
+				best_dist = dist;
+			}
+		}
+
+		this.processing_pos = this._chunk_queue[best_index];
+		this._chunk_queue.splice(best_index,1);
+
+		this.is_processing_chunk = true;
 	}
 
 
