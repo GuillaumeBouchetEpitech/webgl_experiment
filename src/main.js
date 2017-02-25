6c9322ecab9d25a6a028b77f853d6c8fef6ee873
GuillaumeBouchetEpitech <guillaume.bouchet@epitech.eu>
2017-02-25 08:36:38 +0000

    use and test of the new refractored main class

--- src/main.js
+++ src/main.js
@@ -2,3 +2,16 @@
 var createWebGLExperiment = require('./experiment/index.js');
 
 var WebGLExperiment = new createWebGLExperiment();
+
+WebGLExperiment.start();
+
+var gui_toggle_start = document.getElementById("gui_toggle_start");
+gui_toggle_start.addEventListener('click', function ()
+{
+	console.log('toggle_start');
+
+	if (WebGLExperiment.isRunning())
+		WebGLExperiment.stop();
+	else
+		WebGLExperiment.start();
+});
